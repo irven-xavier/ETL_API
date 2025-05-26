6e2a54480442305968beb1ecb86d033832d7d209
irven-xavier <irven.xavier@gmail.com>
2025-05-26 11:38:20 -0300

    load.py atualizado: adição de controle de erros e logging

--- src/load.py
+++ src/load.py
@@ -1,29 +1,40 @@
 from src.create_db import engine, meu_db
 from sqlalchemy.dialects.postgresql import insert
+import logging
 
 def carregar_dados_bulk_upsert(dados):
 
-    data_dicts = [
+    try:
 
-        {
-            "id": d.id,
-            "data": d.data,
-            "categoria": d.categoria,
-            "valor": d.valor,
-            "parcelas": d.parcelas,
-            "data_extracao": d.data_extracao,
-        }
+        data_dicts = [
 
-        for d in dados
+            {
+                "id": d.id,
+                "data": d.data,
+                "categoria": d.categoria,
+                "valor": d.valor,
+                "parcelas": d.parcelas,
+                "data_extracao": d.data_extracao,
+            }
 
-    ]
+            for d in dados
+
+        ]
+        
+        stmt = insert(meu_db).values(data_dicts)
+
+        stmt = stmt.on_conflict_do_nothing(index_elements=['id'])
+                                        
+        with engine.begin() as conn:
+
+            conn.execute(stmt)
+
+        logging.info(f"Carregamento no Postgre concluído: {len(data_dicts)} registros processados. \n")
+
+        return True
     
-    stmt = insert(meu_db).values(data_dicts)
+    except Exception as e:
 
-    stmt = stmt.on_conflict_do_nothing(index_elements=['id'])
-                                      
-    with engine.begin() as conn:
+        logging.error(f"Erro no load: {e}")
 
-        conn.execute(stmt)
-
-    print("Carrgamento de dados no PostgreSQL realizado.\n")
+        return False
